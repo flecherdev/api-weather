dfe45a3ebbb086c3d498b1fb0483f2b204e806b7
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2020-08-26 22:16:26 -0300

    se genera metodo getLocation

--- services/ipapi.js
+++ services/ipapi.js
@@ -4,7 +4,7 @@ class IpApiService {
     constructor() {}
 
     async getLocation(ip) {
-        const mip = '181.28.65.11'
+        // const mip = '181.28.65.11'
 
         try {
             const response = await axios.get(`https://ipapi.co/${mip}/json/`);
@@ -13,16 +13,6 @@ class IpApiService {
             console.log(error)
         }
         
-    }
-
-    getCurrent(city) {
-        const data = 'viene de servicios'
-        return Promise.resolve(data)
-    }
-
-    getForecast(ip, city) {
-        const data = 'viene de servicios'
-        return Promise.resolve(data)
     }
 
 }
