ccdd51234209022092752ee2a182a6fcae0d6a58
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2020-08-26 22:22:03 -0300

    se agregan servicios faltantes

--- services/weather.js
+++ services/weather.js
@@ -15,20 +15,44 @@ class WeatherService {
         const response = await axios.get(`
         https://api.openweathermap.org/data/2.5/weather?q=${responseIpApi.city}&appid=${config.key_openWeather}`);
 
-
-        // console.log(response)
         return Promise.resolve(response.data)
     }
 
-    getCurrent(city) {
-        const data = 'viene de servicios'
-        return Promise.resolve(data)
+    async getCurrent(ip, city) {
+        
+        if(!city) {
+            console.log('ciudad vacia busco por ip')
+            const responseIpApi = await this.ipApiService.getLocation(ip)
+            const response = await axios.get(`
+                https://api.openweathermap.org/data/2.5/weather?q=${responseIpApi.city}&appid=${config.key_openWeather}`
+            );
+            return Promise.resolve(response.data)
+        } else {
+            console.log('busco por ciudad')
+            const response = await axios.get(`
+                https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${config.key_openWeather}`
+            );
+            return Promise.resolve(response.data)
+        }
+        
     }
 
-    getForecast(city) {
-        // api.openweathermap.org/data/2.5/forecast/daily?q=London&mode=xml&units=metric&cnt=5
-        const data = 'viene de servicios 5 dias'
-        return Promise.resolve(data)
+    async getForecast(ip, city) {
+        const cnt = 5
+        if(!city) {
+            console.log('ciudad vacia busco por ip')
+            const responseIpApi = await this.ipApiService.getLocation(ip)
+            const response = await axios.get(`
+                http://api.openweathermap.org/data/2.5/forecast?q=${responseIpApi.city}&mode=json&units=metric&cnt=${cnt}&appid=${config.key_openWeather}`
+            );
+            return Promise.resolve(response.data)
+        } else {
+            console.log('busco por ciudad')
+            const response = await axios.get(`
+            http://api.openweathermap.org/data/2.5/forecast?q=${city}&mode=json&units=metric&cnt=${cnt}&appid=${config.key_openWeather}`
+            );
+            return Promise.resolve(response.data)
+        }
     }
 
 }
