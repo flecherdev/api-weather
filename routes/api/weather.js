a4375052905fc51ec56729d4f70edc7cc7425e68
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2020-08-29 17:16:49 -0300

    se realiza cambio de responsabilidad entre router y app

--- routes/api/weather.js
+++ routes/api/weather.js
@@ -1,67 +1,59 @@
 const express = require('express')
-const router = express.Router()
 const WeatherService  = require('../../services/weather')
-const weather = new WeatherService()
 const publicIp = require('public-ip');
 
-/** 
- * 
- * /location  -> Devuelve los datos de ubicación city según ip-api.
- * 
- 
- * /current[/city]  ->  City es un parámetro opcional. Devuelve los datos de ubicación city o la ubicación actual según
-                        ip-api y el estado del tiempo actual.
- * 
-   / forecast[/city]  ->  City es un parámetro opcional. Devuelve los datos de ubicación city o la ubicación actual según
-                          ip-api y el estado del tiempo a 5 días
- * 
- */
+function weatherApi (app){
+    const router = express.Router()
+    const weather = new WeatherService()
+    app.use('/v1', router)
 
-router.get('/location',async function(req, res, next){
-    var ip = await publicIp.v4()
+    router.get('/location',async function(req, res, next){
+        var ip = await publicIp.v4()
+    
+        try {
+            // myUndefinedFunction();
+            const response = await weather.getLocation(ip)
+            res.status(200).json({
+                data: response,
+                ip: ip,
+                message: 'location'
+            })
+        } catch (error) {
+            next(error)
+        }
+    
+    })
+    
+    router.get('/current/:city?', async function(req, res, next){
+        var ip = await publicIp.v4()
+        const { city } = req.params
+    
+        try {
+            const response = await weather.getCurrent(ip, city) 
+            res.status(200).json({
+                data: response,
+                message: 'current'
+            })
+        } catch (error) {
+            next(error)
+        }
+    })
+    
+    router.get('/forecast/:city?', async function(req, res, next){
+        var ip = await publicIp.v4()
+        const { city } = req.params
+    
+        try {
+            const response = await weather.getForecast(ip, city)
+            res.status(200).json({
+                data: response,
+                message: 'current'
+            })
+        } catch (error) {
+            next(error)
+        }
+    })
+}
 
-    try {
-        // myUndefinedFunction();
-        const response = await weather.getLocation(ip)
-        res.status(200).json({
-            data: response,
-            ip: ip,
-            message: 'location'
-        })
-    } catch (error) {
-        next(error)
-    }
 
-})
-
-router.get('/current/:city?', async function(req, res, next){
-    var ip = await publicIp.v4()
-    const { city } = req.params
-
-    try {
-        const response = await weather.getCurrent(ip, city) 
-        res.status(200).json({
-            data: response,
-            message: 'current'
-        })
-    } catch (error) {
-        next(error)
-    }
-})
-
-router.get('/forecast/:city?', async function(req, res, next){
-    var ip = await publicIp.v4()
-    const { city } = req.params
-
-    try {
-        const response = await weather.getForecast(ip, city)
-        res.status(200).json({
-            data: response,
-            message: 'current'
-        })
-    } catch (error) {
-        next(error)
-    }
-})
-
-module.exports = router
+module.exports = weatherApi
