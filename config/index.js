5022f04f4db45283ca4e3ccf29996dc3749b26a1
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2020-08-30 14:53:32 -0300

    modificacion en port y configuracion

--- config/index.js
+++ config/index.js
@@ -2,10 +2,11 @@ require('dotenv').config()
 
 const config = {
     dev: process.env.NODE_ENV !== 'production',
+    port: process.env.PORT,
     key_openWeather: process.env.KEY_OPENWEATHER,
     sentryDns: process.env.SENTRY_DNS,
     sentryId: process.env.SENTRY_ID
 }
 
-module.exports = {  config: config };
+module.exports = {  config };
 
